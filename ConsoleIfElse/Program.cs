57a7452c683658501a1e7e238e0b84de8f0b5629
marigold <aj.appe@mail.ru>
2023-03-01 22:55:50 +0300

    Добавили консоль с условием

---
+++ ConsoleIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите свое имя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ева")
+{
+    Console.WriteLine("Приветствую Вас, императрица Ева");
+}
+else
+{
+    Console.Write("Привет, плебей ");
+    Console.Write(username);
+}
